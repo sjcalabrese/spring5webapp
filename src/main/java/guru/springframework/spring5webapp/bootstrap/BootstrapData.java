1c7f41579cf5bf587ae9e1db81c2412d86e8f1e4
Sal Calabrese <erotavlas103@gmail.com>
2022-12-15 16:48:53 -0500

    Creating publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRrepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +16,14 @@ public class BootstrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
 
+    private final PublisherRrepository publisherRrepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRrepository publisherRrepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRrepository = publisherRrepository;
+
     }
 
     @Override
@@ -40,7 +46,13 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Publisher guruPublishing = new Publisher("guruPublishing", "123 mySteet", "nowhere", "virginia", "12321");
+
+        publisherRrepository.save(guruPublishing);
+
+
         System.out.println("Sarted in Bootstrap");
         System.out.println("number of books: " + bookRepository.count());
+        System.out.println("Number of publishers: " + publisherRrepository.count());
     }
 }
